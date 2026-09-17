bdcd1dfc3cd63b1d77dc8eb4264ce88106070a4f
SAHIL R. <147610938+ThatOn3Gu7@users.noreply.github.com>
2026-09-17 22:32:05 +0530

    Prepare MindRelay project for repository transfer

--- app/src/main/kotlin/quiz/thaton3app/nazo/data/UpdateFrequency.kt
+++
@@ -1,3 +0,0 @@
-package quiz.thaton3app.nazo.data
-
-enum class UpdateFrequency { EVERY_LAUNCH, WEEKLY, BI_WEEKLY, NEVER }
--- app/src/main/kotlin/quiz/thaton3app/nazo/ui/components/FloatingParticlesBackground.kt
+++
@@ -1,9 +0,0 @@
-package quiz.thaton3app.nazo.ui.components
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun FloatingParticlesBackground(modifier: Modifier = Modifier) {
-    AmbientBackground(modifier = modifier, style = "shapes")
-}
