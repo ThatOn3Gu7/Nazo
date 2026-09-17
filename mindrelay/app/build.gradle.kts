4ed29e96dec42128448d9b5ed774fb15072aca8f
SAHIL R. <147610938+ThatOn3Gu7@users.noreply.github.com>
2026-09-17 22:18:40 +0530

    Raise compileSdk for current Expressive dependencies

--- mindrelay/app/build.gradle.kts
+++ mindrelay/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sahil.mindrelay"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.sahil.mindrelay"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"
     }
